e968d40e09c26ffafd013f35573579f2ea4d482e
rwtmeghna <mr7611232@gmail.com>
2025-08-16 11:34:55 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
+          {process.env.React_app || "Github Actions Tutorial"} - By
           DevOps Pro
         </p>
         <a
